570f5a4a220570e8b466183819c824390c62db96
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2022-12-02 15:44:08 +0300

    <type a commit message>

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Code your solution in this file
-let companyName = 'Scuber';
-const companyName = {'Scuber'};
+const companyName = 'Scuber';
 
+let mostProfitableNeighborhood = 'Chelsea';
 
+let companyCeo = 'Susan Smith';
 
 
 
